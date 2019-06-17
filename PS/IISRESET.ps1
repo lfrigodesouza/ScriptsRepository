e1856b4be0885aadf876a09ff18e77a1681dd883
Lucas Frigo de Souza <lucas.souza@ppi-multitask.com.br>
2019-06-17 14:16:12 -0300

    Adição de scripts PS

---
+++ PS/IISRESET.ps1
@@ -0,0 +1,23 @@
+﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+IISRESET
+#Script para mover os arquivos de log para uma pasta com data-hora
+$logdir="C:\PCF\LOG\4"
+$newdir="$logdir\$(get-date -Format 'yyyyMMdd_HHmmss')"
+push-location $logdir;
+Write-Host $newdir
+New-Item $newdir -type directory
+get-childitem *.log,*.err,*.txt | foreach-object {move-item $_ -destination $newdir}
+Move-Item -Path "$logdir\Site*" $newdir -Force
+
+#Script para apagar as pastas, mantendo somente as 3 ultimas
+#Mantem 3 dias de log em arquivo
+gci $logdir -Recurse -Filter "201*" |
+where{$_.PsIsContainer -and $_.lastwritetime -le (get-date).adddays(-4)}|
+sort CreationTime|
+Remove-Item -Force -Recurse
---
+++ PS/StopStartPCFApplicationService.ps1
@@ -0,0 +1,26 @@
+﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+$ServiceName = 'PCFApplicationServer'
+$arrService = Get-Service -Name $ServiceName
+if($arrService.Status -eq 'Running')
+{
+
+	$confirmation = Read-Host "O serviço está executando. Deseja pará-lo? (S/n)"
+	if($confirmation -eq 's' -or  $confirmation -eq '') {
+		echo 'Stopping service...'
+        $id = Get-WmiObject -Class Win32_Service -Filter "Name LIKE '$ServiceName'" | Select-Object -ExpandProperty ProcessId
+        $process = Get-Process -Id $id
+		$process.Kill()
+	}
+}else {
+	$confirmation = Read-Host "O serviço está parado. Deja iniciá-lo? (S/n)"
+	if($confirmation -eq 's' -or $confirmation -eq '') {
+		echo 'Starting service...'
+		$arrService | Start-Service
+	}    
+}
